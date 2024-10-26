ed35100248cf6961c01e7a63a251fdb61c0fd6f3
platterss <78766794+platterss@users.noreply.github.com>
2024-10-25 21:48:41 -0700

    Made comments better because they bothered me

--- orbitSim.py
+++ orbitSim.py
@@ -1,52 +1,58 @@
 import math
-#vars
-r = 0 #radius
-r2 = r * 1.1 #radius 2
-v = 0 #velocity
-a = 0 #acceleration
-axis = 0 #axis
-t = 0 #period
-fg = 0 #gravitational force
-m1 = 0 #sun mass
-m2 = 0 #planet mass
-G = 6.67e-11 #gravitational constant
+
+r = 0  # Radius
+r2 = r * 1.1  # Radius 2
+v = 0  # Velocity
+a = 0  # Acceleration
+axis = 0  # Axis
+t = 0  # Period
+fg = 0  # Gravitational force
+m1 = 0  # Sun mass
+m2 = 0  # Planet mass
+G = 6.67e-11  # Gravitational constant
 
 
-#functions
+# Orbital velocity
 def orbVel(m1, m2, r, axis):
-    #orbital velocity
     v = math.sqrt(G * (m1 + m2) * (2 / r - 1 / axis))
     return v
 
+
+# Ellipse axis
 def ellipse(r, r2):
-    #ellipse axis
     area = math.pi * r * r2
     axis = (area)/(math.pi * r)
     return axis
 
+
+# Gravitational force
 def grav_force(m1, m2, r):
-    #gravitational force
     fg = (m1*m2)/(r**2)
     return fg
 
+
+# Acceleration
 def acceleration(m1, m2, r):
-    #acceleration
     a = grav_force(m1, m2, r) / m2
     return a
 
+
+# Total energy
 def energy(m1, m2, axis):
-    #total energy
     return -G * (m1 * m2) / 2 * axis
 
+
+# Kinetic energy
 def kinetic(m1, m2, r):
-    #kinetic energy
     return G * (m1 * m2) / 2 * r
 
+
+# Potential energy
 def potential(m1, m2, r):
-    #potential energy
     return -G * (m1 * m2) / r
 
+
+# Period
 def period(m1, m2, axis):
-    #period
     t = 2 * math.pi * math.sqrt(G *axis**3 / (m1 + m2))
     return t
